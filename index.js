7f51f5aabd7cd527d0e6a89d30b0a73782fa8246
ro-fernandez <rocio2002@live.com.ar>
2022-06-19 18:46:51 -0300

    fifth commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const session = require('express-session');
+const app = express();
 const hbs = require('hbs');
 
-const app = express();
 const port = process.env.PORT;
 
 app.set('view engine', 'hbs');
@@ -11,13 +10,10 @@ hbs.registerPartials(__dirname + '/views/partials');
 
 app.use('/assets', express.static(__dirname + '/public'));
 app.use(express.static('public'));
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(require('./router/sesion'));
 app.use(require('./router/contacto'));
 app.use(require('./router/router'));
+
 
 app.listen(port, () =>{
     console.log(`Usando el puerto http://localhost:${port}`);
