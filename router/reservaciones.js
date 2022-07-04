3c3577a798e623e7a7c8e14668035725814f484a
ro-fernandez <rocio2002@live.com.ar>
2022-07-04 11:50:53 -0300

    update commit

--- router/reservaciones.js
+++ router/reservaciones.js
@@ -1,126 +1,126 @@
 const { Router } = require('express');
 const router  = new Router();
 
-// const mysql = require('mysql');
-// const session = require('express-session');
+const mysql = require('mysql');
+const session = require('express-session');
 
-// // Sesiones
+// Sesiones
 
-// router.use(session({
-//     secret: '123456',
-//     resave: true,
-//     saveUninitialized: true
-// }));
+router.use(session({
+    secret: '123456',
+    resave: true,
+    saveUninitialized: true
+}));
 
 router.get('/reservaciones', (req, res) => {
-//     if(req.session.user != null){
-//         res.redirect('solicitudes');
-//     }
-//     else{
+    if(req.session.user != null){
+        res.redirect('solicitudes');
+    }
+    else{
         res.render('reservaciones', {
             titulo: 'Fotografía | Iniciar Sesión'
         });
-//     }
+    }
 });
 
-// router.post('/registro', (req, res) => {
-//     req.session.regenerate(function (err) {
-//         if (err) next(err)
-//         req.session.user = req.body;
-//         req.session.save(function (err) {
-//             if (err) return next(err)
-//             res.redirect('solicitudes');
-//         })
-//     })
-// });
+router.post('/registro', (req, res) => {
+    req.session.regenerate(function (err) {
+        if (err) next(err)
+        req.session.user = req.body;
+        req.session.save(function (err) {
+            if (err) return next(err)
+            res.redirect('solicitudes');
+        })
+    })
+});
 
-// router.post('/logout', function (req, res, next) {
-//     req.session.user = null
-//     req.session.save(function (err) {
-//       if (err) next(err)
-//       req.session.regenerate(function (err) {
-//         if (err) next(err)
-//         res.redirect('reservaciones')
-//       })
-//     })
-// });
+router.post('/logout', function (req, res, next) {
+    req.session.user = null
+    req.session.save(function (err) {
+      if (err) next(err)
+      req.session.regenerate(function (err) {
+        if (err) next(err)
+        res.redirect('reservaciones')
+      })
+    })
+});
 
-// // Conexión a base de datos
+// Conexión a base de datos
 
-// const conn = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'fernandez_rocio'
-// });
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'fernandez_rocio'
+});
 
-// conn.connect((err) => {
-//     if(err) throw err;
-//     console.log("CONEXIÓN ESTABLECIDA");
-// });
+conn.connect((err) => {
+    if(err) throw err;
+    console.log("CONEXIÓN ESTABLECIDA");
+});
 
-// // Select de las solicitudes de la sesión iniciada
+// Select de las solicitudes de la sesión iniciada
 
-// router.get('/solicitudes', (req, res) => {
-//     const user = req.session.user;
-//     if(user == null){
-//         res.redirect('reservaciones');
-//     }
-//     else{
-//         let sql = "SELECT * FROM solicitudes WHERE email = '" + user.email + "' AND dni = '" + user.dni + "'";
-//         let query = conn.query(sql, (err, results) => {
-//             if(err) throw err;
-//             res.render('../views/solicitudes', {
-//                 titulo: 'Fotografía | Reservas',
-//                 user,
-//                 results: results
-//             });
-//         });
-//     }
-// });
+router.get('/solicitudes', (req, res) => {
+    const user = req.session.user;
+    if(user == null){
+        res.redirect('reservaciones');
+    }
+    else{
+        let sql = "SELECT * FROM solicitudes WHERE email = '" + user.email + "' AND dni = '" + user.dni + "'";
+        let query = conn.query(sql, (err, results) => {
+            if(err) throw err;
+            res.render('../views/solicitudes', {
+                titulo: 'Fotografía | Reservas',
+                user,
+                results: results
+            });
+        });
+    }
+});
 
-// // Insert de nueva solicitud de la sesión iniciada
+// Insert de nueva solicitud de la sesión iniciada
 
-// router.post('/save', (req, res) => {
-//     let data = {email: req.body.email, dni: req.body.dni, apynom: req.body.apynom, promo: req.body.promo, mes: req.body.mes, quincena: req.body.quincena, dia: req.body.dia, horario: req.body.horario, locacion: req.body.locacion};
-//     let sql = "INSERT INTO solicitudes SET ?";
-//     let query = conn.query(sql, data, (err, results) => {
-//         if(err) throw err;
-//         res.redirect('/solicitudes');
-//     });
-// });
+router.post('/save', (req, res) => {
+    let data = {email: req.body.email, dni: req.body.dni, apynom: req.body.apynom, promo: req.body.promo, mes: req.body.mes, quincena: req.body.quincena, dia: req.body.dia, horario: req.body.horario, locacion: req.body.locacion};
+    let sql = "INSERT INTO solicitudes SET ?";
+    let query = conn.query(sql, data, (err, results) => {
+        if(err) throw err;
+        res.redirect('/solicitudes');
+    });
+});
 
-// // Update de solicitud existente de la sesión iniciada
+// Update de solicitud existente de la sesión iniciada
 
-// router.post('/update', (req, res) => {
-//     let sql = "UPDATE solicitudes SET email = '" + req.body.email + "', dni = '" + req.body.dni + "', apynom = '" + req.body.apynom + "', promo = '" + req.body.promo + "', mes = '" + req.body.mes + "', quincena = '" + req.body.quincena + "', dia = '" + req.body.dia + "', horario = '" + req.body.horario + "', locacion = '" + req.body.locacion + "'WHERE id_solicitud =" + req.body.id_solicitud;
-//     let query = conn.query(sql, (err, results) => {
-//         if(err) throw err;
-//         res.redirect('/solicitudes');
-//     });
-// });
+router.post('/update', (req, res) => {
+    let sql = "UPDATE solicitudes SET email = '" + req.body.email + "', dni = '" + req.body.dni + "', apynom = '" + req.body.apynom + "', promo = '" + req.body.promo + "', mes = '" + req.body.mes + "', quincena = '" + req.body.quincena + "', dia = '" + req.body.dia + "', horario = '" + req.body.horario + "', locacion = '" + req.body.locacion + "'WHERE id_solicitud =" + req.body.id_solicitud;
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.redirect('/solicitudes');
+    });
+});
 
-// // Delete de solicitud existente de la sesión iniciada
+// Delete de solicitud existente de la sesión iniciada
 
-// router.post('/delete', (req, res) => {
-//     let sql = "DELETE from solicitudes WHERE id_solicitud =" + req.body.id_solicitud;
-//     let query = conn.query(sql, (err, results) => {
-//         if(err) throw err;
-//         res.redirect('/solicitudes');
-//     });
-// });
+router.post('/delete', (req, res) => {
+    let sql = "DELETE from solicitudes WHERE id_solicitud =" + req.body.id_solicitud;
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.redirect('/solicitudes');
+    });
+});
 
-// // Muestra de solicitudes total para administrador de la página
+// Muestra de solicitudes total para administrador de la página
 
-// router.get('/solicitudes43875244', (req, res) => {
-//     let sql = "SELECT * FROM solicitudes";
-//     let query = conn.query(sql, (err, results) => {
-//         if(err) throw err;
-//         res.render('../views/solicitudes43875244', {
-//             titulo: 'Fotografía | Reservas Totales',
-//             results: results
-//         });
-//     });
-// });
+router.get('/solicitudes43875244', (req, res) => {
+    let sql = "SELECT * FROM solicitudes";
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.render('../views/solicitudes43875244', {
+            titulo: 'Fotografía | Reservas Totales',
+            results: results
+        });
+    });
+});
 
 module.exports = router;
